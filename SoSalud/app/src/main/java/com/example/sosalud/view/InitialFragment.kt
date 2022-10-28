6810b1ab7a4f956e539558a442942848e255ad47
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2022-10-27 23:57:54 -0600

    3 UI navigation completed

--- SoSalud/app/src/main/java/com/example/sosalud/view/InitialFragment.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/view/InitialFragment.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.example.sosalud.databinding.FragmentLoginBinding
+import com.example.sosalud.R
+import com.example.sosalud.databinding.InitialActivityBinding
 
 class InitialFragment : Fragment(){
-    private var _binding: FragmentLoginBinding? = null
+    val button = view?.findViewById<Button>(R.id.button)
+
+    private var _binding: InitialActivityBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,11 +31,20 @@ class InitialFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         // Retrieve and inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
+        val root = inflater.inflate(R.layout.initial_activity, container, false)
+        setupNavigation(root)
+        return root
     }
-
+    private fun setupNavigation(root: View) {
+        val loginButton = root.findViewById<Button>(R.id.button2)
+        val singUpButton = root.findViewById<Button>(R.id.button)
+        loginButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_InitialFragment_to_login_fragment)
+        }
+        singUpButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_InitialFragment_to_activity_register)
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
