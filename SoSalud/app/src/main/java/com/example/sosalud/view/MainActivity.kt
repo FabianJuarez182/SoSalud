97a07b6679d6a9f0da26925fd2bd0aaee9e139b8
Fabian Juarez <83832041+FabianJuarez182@users.noreply.github.com>
2022-10-27 20:19:11 -0600

    start adding navigation

--- SoSalud/app/src/main/java/com/example/sosalud/view/MainActivity.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/view/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.sosalud.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.sosalud.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
