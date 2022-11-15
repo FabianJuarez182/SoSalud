d4dca0dfcd4dbd8dad89e6d0b367bd468a75880d
MelissaPerez09 <per21385@uvg.edu.gt>
2022-11-14 22:45:47 -0600

    drawables modification

--- SoSalud/app/src/main/java/com/example/sosalud/data/DataServices.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/data/DataServices.kt
@@ -7,16 +7,16 @@ class DataServices {
 
     fun loadServices(): List<Services>{
         return listOf<Services>(
-            Services(R.string.Service1, R.drawable.tomaPresion),
-            Services(R.string.Service2, R.drawable.tomaAzucar),
+            Services(R.string.Service1, R.drawable.tomapresion),
+            Services(R.string.Service2, R.drawable.tomaazucar),
             Services(R.string.Service3, R.drawable.inyeccion),
-            Services(R.string.Service4, R.drawable.chequeoCardiaco),
-            Services(R.string.Service5, R.drawable.chequeoGeneral),
-            Services(R.string.Service6, R.drawable.chequeoVision),
-            Services(R.string.Service7, R.drawable.pruebaCovid),
-            Services(R.string.Service8, R.drawable.muestrasLaboratorio),
+            Services(R.string.Service4, R.drawable.chequeocardiaco),
+            Services(R.string.Service5, R.drawable.chequeogeneral),
+            Services(R.string.Service6, R.drawable.chequeovision),
+            Services(R.string.Service7, R.drawable.pruebacovid),
+            Services(R.string.Service8, R.drawable.muestraslaboratorio),
             Services(R.string.Service9, R.drawable.odontologia),
-            Services(R.string.Service10, R.drawable.rayosX)
+            Services(R.string.Service10, R.drawable.rayosx)
         )
     }
 }
