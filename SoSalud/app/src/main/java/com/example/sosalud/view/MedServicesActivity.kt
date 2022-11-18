1cb0918e74ebb3098cf5fc436b2e178af3fb3ac0
MelissaPerez09 <per21385@uvg.edu.gt>
2022-11-18 07:43:08 -0600

    bugs fixed Med Services

--- SoSalud/app/src/main/java/com/example/sosalud/view/MedServicesActivity.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/view/MedServicesActivity.kt
@@ -4,36 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import com.example.sosalud.data.DataServices
-import com.example.sosalud.databinding.FragmentLoginBinding
+import com.example.sosalud.R
 
-class MedServicesActivity: Fragment() {
-    private var _binding: FragmentLoginBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    private lateinit var recyclerView: RecyclerView
-    // Keeps track of which LayoutManager is in use for the [RecyclerView]
-    private var isLinearLayoutManager = true
-
-    override fun onCreateView(
+class MedServicesActivity {
+    fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val myDataset = DataServices().loadServices()
-        // Retrieve and inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return inflater.inflate(R.layout.fragment_medservices, container, false)
     }
 }
