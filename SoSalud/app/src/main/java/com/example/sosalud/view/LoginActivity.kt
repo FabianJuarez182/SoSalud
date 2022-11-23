2a2bd1c508d0eebfac0320275511e7913d5c2bef
MelissaPerez09 <per21385@uvg.edu.gt>
2022-11-22 23:40:27 -0600

    firebase started

--- SoSalud/app/src/main/java/com/example/sosalud/view/LoginActivity.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/view/LoginActivity.kt
@@ -1,44 +1,86 @@
 package com.example.sosalud.view
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
 import com.example.sosalud.R
 import com.example.sosalud.databinding.FragmentLoginBinding
+import com.example.ui.auth.AuthUI
+import com.example.sosalud.auth.FirebaseAuthUIActivityResultContract
+import com.example.ui.auth.IdpResponse
+import com.example.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.codelab.friendlychat.databinding.ActivitySignInBinding
+import com.google.firebase.ktx.Firebase
 
-class LoginActivity : Fragment() {
-    private var _binding: FragmentLoginBinding? = null
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: FragmentLoginBinding
 
-    private val binding get() = _binding!!
+    private val signIn: ActivityResultLauncher<Intent> =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract(), this::onSignInResult)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Retrieve and inflate the layout for this fragment
-        _binding = FragmentLoginBinding.inflate(inflater, container, false)
-        val view = binding.root
-        return view
-        val root = inflater.inflate(R.layout.fragment_login, container, false)
-        setupNavigation(root)
-        return root
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // This codelab uses View Binding
+        // See: https://developer.android.com/topic/libraries/view-binding
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
-    private fun setupNavigation(root: View) {
-        val loginButton2 = root.findViewById<Button>(R.id.button3)
-        loginButton2.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_login_fragment_to_fragment_medservices)
+    public override fun onStart() {
+        super.onStart()
+
+        // If there is no signed in user, launch FirebaseUI
+        // Otherwise head to MainActivity
+        if (Firebase.auth.currentUser == null) {
+            // Sign in with FirebaseUI, see docs for more details:
+            // https://firebase.google.com/docs/auth/android/firebaseui
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setLogo(R.mipmap.ic_launcher)
+                .setAvailableProviders(listOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                ))
+                .build()
+
+            signIn.launch(signInIntent)
+        } else {
+            goToMainActivity()
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        if (result.resultCode == RESULT_OK) {
+            Log.d(TAG, "Sign in successful!")
+            goToMainActivity()
+        } else {
+            Toast.makeText(
+                this,
+                "There was an error signing in",
+                Toast.LENGTH_LONG).show()
+
+            val response = result.idpResponse
+            if (response == null) {
+                Log.w(TAG, "Sign in canceled")
+            } else {
+                Log.w(TAG, "Sign in error", response.error)
+            }
+        }
+    }
+
+    private fun goToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    companion object {
+        private const val TAG = "SignInActivity"
     }
 }
 
