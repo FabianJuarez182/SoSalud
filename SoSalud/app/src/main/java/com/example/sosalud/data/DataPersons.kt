487c4edd9ed2d7f036cea1ea2363df1255b8d5f6
MelissaPerez09 <per21385@uvg.edu.gt>
2022-11-25 00:49:11 -0600

    navigation finished

--- SoSalud/app/src/main/java/com/example/sosalud/data/DataPersons.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/data/DataPersons.kt
@@ -8,7 +8,12 @@ class DataPersons {
 
     fun loadPersons(): List<Persons>{
         return listOf<Persons>(
-            Persons(R.string.Person1,R.string.Hour1,R.string.Addres1,R.drawable.fernanda)
+            Persons(R.string.Person1,R.string.Hour1,R.string.Addres1,R.drawable.fernanda),
+            Persons(R.string.Person2,R.string.Hour2,R.string.Addres2,R.drawable.marcos),
+            Persons(R.string.Person3,R.string.Hour3,R.string.Addres3,R.drawable.jose),
+            Persons(R.string.Person4,R.string.Hour4,R.string.Addres4,R.drawable.javier),
+            Persons(R.string.Person5,R.string.Hour5,R.string.Addres5,R.drawable.clarissa),
+            Persons(R.string.Person6,R.string.Hour6,R.string.Addres6,R.drawable.sofia)
         )
     }
 }
--- SoSalud/app/src/main/java/com/example/sosalud/view/HomecareActivity.kt
+++ SoSalud/app/src/main/java/com/example/sosalud/view/HomecareActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sosalud.R
@@ -16,13 +18,26 @@ import com.example.sosalud.databinding.FragmentHomecareBinding
 
 class HomecareActivity: Fragment() {
     private var _binding: FragmentHomecareBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_homecare, container, false)
+        _binding = FragmentHomecareBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+        val root = inflater.inflate(R.layout.fragment_homecare, container, false)
+        setupNavigation(root)
+        return root
+    }
+
+    private fun setupNavigation(root: View) {
+        val atender = root.findViewById<Button>(R.id.button6)
+        atender.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_fragment_homecare_to_fragment_medservices)
+        }
     }
 
     override fun onViewCreated(
@@ -33,6 +48,9 @@ class HomecareActivity: Fragment() {
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewHomeCare)
         recyclerView.layoutManager = LinearLayoutManager(view.context)
         recyclerView.adapter = HomecareAdapter(this, HomecareServices)
+        binding.button6.setOnClickListener {
+            view.findNavController().navigate(R.id.action_fragment_homecare_to_fragment_medservices)
+        }
     }
 
     override fun onDestroyView() {
